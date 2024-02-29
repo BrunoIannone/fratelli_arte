2c2fd35d81c95f8e5b0e49c75a90eb5db3fb75fc
Bruno_Iannone <iannone.1883472@studenti.uniroma1.it>
2024-02-29 22:16:07 +0100

    Fixed date. Added shutdown

---
+++ src/css/shutdown_btn/shutdown_btn.tsx
@@ -0,0 +1,39 @@
+import { Component } from 'react';
+import toastr from "toastr";
+
+interface ShutdownButtonProps {
+  imageUrl: string;
+  
+}
+
+class ShutdownButton extends Component<ShutdownButtonProps> {
+  sendShutdownRequest = () => {
+    fetch("http://192.168.1.18:3000/shutdown", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ action: 'shutdown' }) // You might need to adjust this payload based on your server's requirements
+    })
+    .then(response => {
+      // Handle response as needed
+      toastr.success('Richiesta inviata con successo.', "Invio richiesta di spegnimento", { closeButton: true, progressBar: true, timeOut: 5000, extendedTimeOut: 2000});
+    })
+    .catch(error => {
+      // Handle error
+      toastr.error('Invio richiesta di spegnimento fallita:', error, { closeButton: true, progressBar: true, timeOut: 5000, extendedTimeOut: 2000});
+    });
+  };
+
+  render() {
+    return (
+      <div className='shutdown-button-container'>
+      <button className="shutdown-button" onClick={this.sendShutdownRequest}>
+        <img src={this.props.imageUrl} alt="Shutdown Button" />
+      </button>
+      </div>
+    );
+  }
+}
+
+export default ShutdownButton;
